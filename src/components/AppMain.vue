253568224482b20d9d51c078ab924b359e075b5d
DarioGuastella <jimmyguastella@gmail.com>
2023-12-11 18:10:10 +0100

    aggiunge sezione main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -26,12 +26,25 @@ export default {
 </script>
 
 <template>
-    <Slider />
+    <Slider /><!-- ToDo: slider content e fade -->
+    <section class="container-fluid bg-light py-2 w-100"> <!-- ToDo: hover -->
+        <div class="row">
+            <div class="col-3 px-2"><img class="quarterImg" src="/img/h3-img-1.jpg" alt=""></div>
+            <div class="col-3 px-2"><img class="quarterImg" src="/img/h3-img-2.jpg" alt=""></div>
+            <div class="col-3 px-2"><img class="quarterImg" src="/img/h3-img-3.jpg" alt=""></div>
+            <div class="col-3 px-2"><img class="quarterImg" src="/img/h3-img-4.jpg" alt=""></div>
+        </div>
+
+    </section>
 </template>
 
 <style scoped lang="scss">
 // importo variabili
 // @use './styles/partials/variables' as *;
+.quarterImg {
+    width: 468px;
+    height: 320px;
+}
 
 // ...qui eventuale SCSS di AppComponent
 </style>
