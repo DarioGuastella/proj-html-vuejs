37a542993e91d409289f8c5a71bd0e7e95729cd6
DarioGuastella <jimmyguastella@gmail.com>
2023-12-13 19:37:46 +0100

    aggiunge bottoni animati pizze

--- src/components/AppPizzaSlider.vue
+++ src/components/AppPizzaSlider.vue
@@ -14,9 +14,6 @@ import { Autoplay, Navigation } from 'swiper/modules';
 
 export default {
     name: "AppPizzaSlider",
-    props: {
-        data: Object
-    },
     components: {
         Swiper,
         SwiperSlide,
@@ -36,59 +33,28 @@ export default {
 </script>
 
 <template>
-    <swiper :slidesPerView="5" :spaceBetween="2" :autoplay="{ delay: 2000, disableOnInteraction: false }" :loop="true"
-        :modules="modules" class="mySwiper">
+    <swiper :slidesPerView="5" :spaceBetween="2" :loop="true" :modules="modules" class="mySwiper">
         <swiper-slide v-for="pizze, i in this.store.pizzas" class="text-center">
-            <div class="pizzaBox">
-                <img :src="store.pizzas[i].image" alt="">
+            <div class="pizzaCard">
+                <div class="pizzaBox">
+                    <img :src="store.pizzas[i].image" alt="">
+                </div>
+                <h3 class="mt-4 pizzaText">{{ store.pizzas[i].name }}</h3>
+                <h3 class="pizzaPrice"><span>{{ store.pizzas[i].exprice }}</span>{{ store.pizzas[i].price }}</h3>
+                <div class="d-flex">
+                    <button class="viewBtn button me-2">ADD TO CART</button>
+                    <button class="quickBtn button">QUICK VIEW</button>
+                </div>
             </div>
-            <h3 class="mt-4 pizzaText">{{ store.pizzas[i].name }}</h3>
-            <h3 class="pizzaPrice"><span>{{ store.pizzas[i].exprice }}</span>{{ store.pizzas[i].price }}</h3>
-        </swiper-slide>
-        <!-- <swiper-slide class="text-center">
-            <div class="pizzaBox">
-                <img src="/img/h3-product-img-2a.png" alt="">
 
-            </div>
-            <h3 class="mt-4 pizzaText">FIORI DI ZUCCA</h3>
-            <h3 class="pizzaPrice">$7.00 - $50.00</h3>
         </swiper-slide>
-
-        <swiper-slide class="text-center">
-            <div class="pizzaBox">
-                <img src="/img/h3-product-img-3a.png" alt="">
-            </div>
-            <h3 class="mt-4 pizzaText">CAMPAGNOLA</h3>
-            <h3 class="pizzaPrice">$50.00 - $95.00</h3>
-        </swiper-slide>
-        <swiper-slide class="text-center">
-            <div class="pizzaBox">
-                <img src="/img/h3-product-img-4a.png" alt="">
-            </div>
-            <h3 class="mt-4 pizzaText">PIZZA TARTUFATA</h3>
-            <h3 class="pizzaPrice">$45.00</h3>
-        </swiper-slide>
-        <swiper-slide class="text-center">
-            <div class="pizzaBox">
-                <img src="/img/h3-product-img-5a.png" alt="">
-            </div>
-            <h3 class="mt-4 pizzaText">FRANCESCANA</h3>
-            <h3 class="pizzaPrice">$25.00</h3>
-        </swiper-slide>
-        <swiper-slide class="text-center">
-            <div class="pizzaBox">
-                <img src="/img/h3-product-img-6a.png" alt="">
-            </div>
-            <h3 class="mt-4 pizzaText">VALDOSTANA</h3>
-            <h3 class="pizzaPrice">$55.00</h3>
-        </swiper-slide> -->
-
+        <!-- :autoplay="{ delay: 2000, disableOnInteraction: false }" -->
     </swiper>
 </template>
 <style scoped lang="scss">
 .swiper-slide {
     width: 200px;
-    height: auto;
+    height: 450px;
 
     /* Center slide text vertically */
     img {
@@ -108,6 +74,16 @@ export default {
         }
     }
 
+    // .button {
+    //     display: none;
+    // }
+
+
+
+}
+
+.swiper-slide:hover .button {
+    display: flex;
 }
 
 .pizzaPrice {
@@ -115,13 +91,100 @@ export default {
 }
 
 .pizzaBox {
-    height: 60%;
-    width: 100%;
+    height: 200px;
+}
+
+.pizzaCard {
+    text-align: center;
+    /* align-content: center; */
+    align-items: center;
+    display: flex;
+    flex-direction: column;
 }
 
 .pizzaText {
     color: #b68a2c;
 
+}
+
+.button {
+    opacity: 0;
+}
+
+.pizzaCard:hover .viewBtn {
+    animation-delay: 0.1s;
+    animation-name: fade;
+    animation-duration: 0.4s;
+    animation-fill-mode: forwards;
+    position: relative;
+    opacity: 0;
+}
+
+.pizzaCard:hover .quickBtn {
+    animation-delay: 0.2s;
+    animation-name: fade;
+    animation-duration: 0.6s;
+    animation-fill-mode: forwards;
+    position: relative;
+    opacity: 0;
+}
+
+@keyframes fade {
+    from {
+        opacity: 0;
+        top: 1rem;
+    }
+
+    to {
+        opacity: 1;
+        top: 0
+    }
+}
+
+.quickBtn {
+    /*Reset*/
+    display: inline-block;
+    outline: none;
+    background-color: white;
+
+    /*Some fancy stuff*/
+    line-height: 200%;
+    border: 0;
+    /*Magic*/
+    background-image: linear-gradient(#f6f7f2, #f6f7f2 50%, #c83b1a 50%, #c83b1a);
+    background-size: 100% 200%;
+    transition: background-position .3s;
+    color: black;
+    padding: 0.5rem 1.6rem;
+    font-size: 0.8rem;
+    font-weight: 500;
+
+    &:hover {
+        background-position: 0 100%;
+    }
+}
+
+.viewBtn {
+    /*Reset*/
+    display: inline-block;
+    outline: none;
+    background-color: white;
+
+    /*Some fancy stuff*/
+    line-height: 200%;
+    border: 0;
+    /*Magic*/
+    background-image: linear-gradient(#d2401e, #d2401e 50%, #c83b1a 50%, #c83b1a);
+    background-size: 100% 200%;
+    transition: background-position .3s;
+    color: white;
+    padding: 0.5rem 1.6rem;
+    font-size: 0.8rem;
+    font-weight: 600;
+
+    &:hover {
+        background-position: 0 100%;
+    }
 }
 </style>
 
